8dc16983c6dfb701f2bf45148e28ddd2501f68a8
Mihai Lungu <lungu.mihai03@gmail.com>
2025-09-05 16:07:29 +0300

    Add project files.

---
+++ About-Pc/Form1.Designer.cs
@@ -0,0 +1,221 @@
+﻿namespace About_Pc
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(9, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "CPU:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(9, 56);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "RAM:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(10, 179);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(63, 25);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "GPU:";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(10, 251);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(67, 25);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "ROM:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(85, 9);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(70, 25);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "label5";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(85, 56);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(70, 25);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "label6";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(85, 179);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(70, 25);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "label7";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(85, 251);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(70, 25);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "label8";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.label9.Font = new System.Drawing.Font("Modern No. 20", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(0, 659);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(180, 25);
+            this.label9.TabIndex = 8;
+            this.label9.Text = "By Mihai Lungu";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.Location = new System.Drawing.Point(13, 384);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(48, 25);
+            this.label10.TabIndex = 9;
+            this.label10.Text = "OS:";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label11.Location = new System.Drawing.Point(85, 384);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(82, 25);
+            this.label11.TabIndex = 10;
+            this.label11.Text = "label11";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label12.Location = new System.Drawing.Point(13, 454);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(148, 25);
+            this.label12.TabIndex = 11;
+            this.label12.Text = "Mother Board:";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label13.Location = new System.Drawing.Point(174, 454);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(82, 25);
+            this.label13.TabIndex = 12;
+            this.label13.Text = "label13";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1292, 684);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Informatii despre calculatorul tau";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label13;
+    }
+}
+
---
+++ About-Pc/Form1.cs
@@ -0,0 +1,303 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Management;
+using System.IO;
+using System.Diagnostics;
+using Microsoft.Win32;
+
+namespace About_Pc
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string cpuInfo = GetCPUInfo();
+            label5.Text = cpuInfo;
+
+            string ramInfo = GetRAMInformation();
+            label6.Text = ramInfo;
+
+            string gpuInfo = GetGPUInfo();
+            label7.Text = gpuInfo;
+
+            string romInfo = GetROMInfo();
+            label8.Text = romInfo;
+
+            string osInfo = GetOSInfo();
+            label11.Text = osInfo;
+
+            string motherboardInfo = GetMotherboardInfo();
+            label13.Text = motherboardInfo;
+        }
+
+
+        private string GetCPUInfo()
+        {
+            string cpuInfo = string.Empty;
+            ManagementClass mc = new ManagementClass("win32_processor");
+            ManagementObjectCollection moc = mc.GetInstances();
+
+            foreach (ManagementObject mo in moc)
+            {
+                string name = mo.Properties["Name"].Value.ToString();
+                int cores = Convert.ToInt32(mo.Properties["NumberOfCores"].Value);
+                int threads = Convert.ToInt32(mo.Properties["NumberOfLogicalProcessors"].Value);
+                cpuInfo += $"{name} (Cores: {cores}, Threads: {threads})";
+                break;
+            }
+
+            return cpuInfo;
+        }
+
+
+
+        private string GetRAMInformation()
+        {
+            string ramInfo = string.Empty;
+            ManagementObjectSearcher searcher = new ManagementObjectSearcher("SELECT * FROM Win32_PhysicalMemory");
+
+            foreach (ManagementObject mo in searcher.Get())
+            {
+                string capacity = mo["Capacity"] != null ? BytesToGB(long.Parse(mo["Capacity"].ToString())) : "Unknown";
+                string speed = mo["Speed"]?.ToString();
+                string memoryType = mo["SMBIOSMemoryType"] != null ? GetSMBIOSMemoryTypeString(int.Parse(mo["SMBIOSMemoryType"].ToString())) : "Unknown";
+
+                
+                string formFactor = mo["FormFactor"] != null ? GetFormFactorString(int.Parse(mo["FormFactor"].ToString())) : "Unknown";
+                string manufacturer = mo["Manufacturer"]?.ToString();
+                string partNumber = mo["PartNumber"]?.ToString();
+
+                ramInfo +=
+                        $"{capacity} GB, " +
+                        $"{memoryType}, " +
+                        $"{speed} MHz, " +
+                        $"{formFactor} " + $"{partNumber}\n";
+            }
+            return ramInfo.Trim();
+        }
+
+        private string BytesToGB(long bytes)
+        {
+            double gb = bytes / (1024.0 * 1024.0 * 1024.0);
+            return gb.ToString("0.##");
+        }
+        private string BytesToMB(long bytes)
+        {
+            double gb = bytes / (1024.0 * 1024.0);
+            return gb.ToString("0.##");
+        }
+        private string GetSMBIOSMemoryTypeString(int memoryType)
+        {
+            switch (memoryType)
+            {
+                case 24: return "DDR3";
+                case 25: return "FBD2";
+                case 26: return "DDR4";
+                // adaugă alte cazuri după nevoie
+                default: return "Unknown";
+            }
+        }
+
+
+       
+
+        private string GetFormFactorString(int formFactor)
+        {
+            switch (formFactor)
+            {
+                case 1:
+                    return "Other";
+                case 2:
+                    return "SIP";
+                case 3:
+                    return "DIP";
+                case 4:
+                    return "ZIP";
+                case 5:
+                    return "SOJ";
+                case 6:
+                    return "Proprietary";
+                case 7:
+                    return "SIMM";
+                case 8:
+                    return "DIMM";
+                case 9:
+                    return "TSOP";
+                case 10:
+                    return "PGA";
+                case 11:
+                    return "RIMM";
+                case 12:
+                    return "SODIMM";
+                case 13:
+                    return "SRIMM";
+                case 14:
+                    return "SMD";
+                case 15:
+                    return "SSMP";
+                case 16:
+                    return "QFP";
+                case 17:
+                    return "TQFP";
+                case 18:
+                    return "SOIC";
+                case 19:
+                    return "LCC";
+                case 20:
+                    return "PLCC";
+                case 21:
+                    return "BGA";
+                case 22:
+                    return "FPBGA";
+                case 23:
+                    return "LGA";
+                default:
+                    return "Unknown";
+            }
+        }
+
+
+        private string GetGPUInfo()
+        {
+            string gpuInfo = string.Empty;
+            ManagementClass mc = new ManagementClass("win32_videocontroller");
+            ManagementObjectCollection moc = mc.GetInstances();
+
+            foreach (ManagementObject mo in moc)
+            {
+                string name = mo.Properties["Name"].Value.ToString();
+                string vram = mo.Properties["AdapterRAM"].Value != null ? BytesToMB(long.Parse(mo.Properties["AdapterRAM"].Value.ToString())) : "Unknown";
+                gpuInfo += $"{name} (VRAM: {vram} MB)" + Environment.NewLine;
+            }
+
+            return gpuInfo.Trim();
+        }
+
+
+
+        private string GetROMInfo()
+        {
+            string romInfo = string.Empty;
+            ManagementClass mc = new ManagementClass("win32_logicaldisk");
+            ManagementObjectCollection moc = mc.GetInstances();
+
+            foreach (ManagementObject mo in moc)
+            {
+                string driveType = mo.Properties["DriveType"].Value.ToString();
+                string driveLetter = mo.Properties["Name"].Value.ToString();
+                string driveLabel = mo.Properties["VolumeName"].Value.ToString();
+                string driveFileSystem = mo.Properties["FileSystem"].Value.ToString();
+                ulong freeSpace = Convert.ToUInt64(mo.Properties["FreeSpace"].Value);
+                string freeSpaceGB = (freeSpace / (1024 * 1024 * 1024)).ToString(); // Convert free space to GB
+
+                if (driveType == "2" || driveType == "3") // DriveType 2 is for removable drives
+                {
+                    string driveTypeString = GetDriveTypeString(driveLetter);
+                    romInfo += $"{driveLetter} ({driveLabel}) - Free Space: {freeSpaceGB} GB,  File System: {driveFileSystem}" + Environment.NewLine;
+                }
+            }
+
+            return romInfo.Trim();
+        }
+
+
+        private string GetDriveTypeString(string driveLetter)
+        {
+            string driveTypeString = "Unknown";
+            ManagementObjectSearcher searcher = new ManagementObjectSearcher("SELECT * FROM Win32_DiskDrive");
+
+            foreach (ManagementObject mo in searcher.Get())
+            {
+                string deviceId = mo["DeviceID"].ToString();
+                string driveLetterId = deviceId.Substring(4, 1);
+
+                if (driveLetter.Equals(driveLetterId, StringComparison.OrdinalIgnoreCase))
+                {
+                    foreach (ManagementObject partition in mo.GetRelated("Win32_DiskPartition"))
+                    {
+                        foreach (ManagementObject logicalDisk in partition.GetRelated("Win32_LogicalDisk"))
+                        {
+                            if (logicalDisk["DeviceID"].ToString().Equals(driveLetter, StringComparison.OrdinalIgnoreCase))
+                            {
+                                string mediaType = mo["MediaType"].ToString().ToLower();
+
+                                if (mediaType.Contains("ssd"))
+                                {
+                                    driveTypeString = "SSD";
+                                }
+                                else if (mediaType.Contains("hdd"))
+                                {
+                                    driveTypeString = "HDD";
+                                }
+                                else
+                                {
+                                    driveTypeString = "Unknown";
+                                }
+
+                                break;
+                            }
+                        }
+                    }
+                    break;
+                }
+            }
+
+            return driveTypeString;
+        }
+
+
+
+
+
+
+
+
+        private string GetOSInfo()
+        {
+            ManagementObjectSearcher mos = new ManagementObjectSearcher("SELECT * FROM Win32_OperatingSystem");
+            ManagementObjectCollection moc = mos.Get();
+
+            string osInfo = string.Empty;
+            foreach (ManagementObject mo in moc)
+            {
+                osInfo = mo["Caption"].ToString() + " " + mo["Version"].ToString();
+                break;
+            }
+
+            return osInfo;
+        }
+        private string GetMotherboardInfo()
+        {
+            string motherboardInfo = string.Empty;
+            ManagementClass mc = new ManagementClass("win32_baseboard");
+            ManagementObjectCollection moc = mc.GetInstances();
+
+            foreach (ManagementObject mo in moc)
+            {
+                motherboardInfo = mo.Properties["Manufacturer"].Value.ToString() + " " +
+                                   mo.Properties["Product"].Value.ToString();
+                break;
+            }
+
+            return motherboardInfo;
+        }
+
+    }
+}
